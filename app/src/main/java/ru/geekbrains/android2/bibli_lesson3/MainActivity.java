fe1738ed2f5baf6d1aad8609d26635b9e9d4de70
koassumo <0770407@gmail.com>
2020-08-04 15:30:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/android2/bibli_lesson3/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.android2.bibli_lesson3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
